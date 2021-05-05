2fe02e68586c267a029308c63920f42f05167799
school-project-crypt <school.project.one.time@gmail.com>
2021-05-05 14:12:46 -0400

    version 0.4

--- scripts/components.js
+++ scripts/components.js
@@ -1,2 +1,23 @@
+let app = {
+  components: [
+    {
+      run: function() {
+        this.sc = "all";
+        this.ti = "navbar";
+        this.code = "test";
+      },
+    },
+  ],
+  run: () => {
+    for (let i = 0; i < app.components.length; i++) {
+      app.components[i].run();
+      switch (app.components[i].sc) {
+        case "all":
+          document.body.innerHTML += app.components[i].code;
+        break;
 
-const topAppBar = new mdc.TopAppBar.MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+        // window.location.URL
+      }
+    }
+  },
+};app.run();
